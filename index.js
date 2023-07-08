3645887e58d51797e71989800685b604f483be0a
MauricioPerezCaram <perezcarammauricio@gmail.com>
2023-07-07 21:06:11 -0300

    ver lo de las mayúsculas y ver cómo agregar function regla de 3 simple

--- index.js
+++ index.js
@@ -15,19 +15,18 @@ while ((usuario !== usuarioValido.toLowerCase() || contrasena !== contrasenaVali
   contrasena = prompt("Ingresa una contraseña de administrador (admin123)").toLowerCase();
 }
 
-
 if (intentos === 2 && (usuario !== usuarioValido || contrasena !== contrasenaValida)) {
 alert("Intentaste muchas veces. Acceso denegado.");
 console.log("Recarga la página");
 } else {
 alert("Bienvenido al programa.");
 let decision = prompt ("Queres saber como organizar tus gastos? Contesta Si o No")
-if (decision == "Si".toLowerCase()){
+if (decision == "Si"){      // cambiar que ignore si esta en mayúsculas o minúsculas
     let sueldo = parseFloat(prompt("Ingresá tu sueldo: "));
     console.log("Teniendo en cuenta que tu sueldo es de $" + sueldo + ", tus gastos deberian dividirse así:");
     gastosCasa = 35*sueldo/100
     gastosVivir = 22*sueldo/100
-    gastosAuto = 15*sueldo/100
+    gastosAuto = 15*sueldo/100      // podría usar function acá? Volver a ver clase para buscar una idea
     gastosEntretenimiento = 8*sueldo/100
     Invertir = 20*sueldo/100
     console.log("Deberías gastar como máximo: $ " + gastosCasa + " en alquiler y demás gastos de la casa. Esto corresponde a un 35% de tu sueldo.");
@@ -36,7 +35,7 @@ if (decision == "Si".toLowerCase()){
     console.log("Deberías gastar como máximo: $ " + gastosEntretenimiento + " en entretenimiento. Esto corresponde a un 8% de tu sueldo.");
     console.log("Deberías gastar como máximo: $ " + Invertir + " en invertir. Esto corresponde a un 20% de tu sueldo.");
     console.log("Debes tener en cuenta que todos estos gastos serían lo ideal para mantener ordenadas las finanzas personales.");
-} else if (decision == "No".toLowerCase()){
+} else if (decision == "No"){       // cambiar que ignore si esta en mayúsculas o minúsculas
     console.log("Elegiste no tener detalle de cómo dividir tus gastos. Fin del programa.");
 } else {
     console.log("Debes ingresar si o no. Recarga la página para volver a empezar.");
